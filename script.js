89f3cbb095492f51e73c776d721247a8f1026a6f
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-03 22:39:07 -0600

    how to use strict

---
+++ script.js
@@ -0,0 +1,13 @@
+/*======== Strict mode ========== */
+
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+//const interface = "Audio";
+
+/*========  HOW TO USE FUNCTIONS ========== */
