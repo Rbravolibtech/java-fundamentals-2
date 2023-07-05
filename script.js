f9a7121df1e5869b136b1a6bedf9b42b57a811eb
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-05 11:02:24 -0600

    ARROW FUNCTIONS

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ if (hasDriversLicense) console.log("I can drive");
 //const interface = "Audio";
 
 /*========  HOW TO USE FUNCTIONS ========== */
-
+/*
 function logger() {
 	console.log("My name is Bravo");
 }
@@ -33,7 +33,7 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 /*========  FUNCTION DECLARATIONS VS EXPRESSIONS ========== */
-
+/*
 //function declaration
 
 function calcAge1(birthYeah) {
@@ -44,9 +44,32 @@ const age1 = calcAge1(1991);
 console.log(age1);
 
 //Function expression
-const calcAge2 = function (birthYeah) {
-	return 2037 - birthYeah;
-};
+//const calcAge2 = function (birthYeah) {
+//return 2037 - birthYeah;
+//};
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+/*========  ARROW FUNCTIONS========== */
+
+//const calcAge2 = function (birthYeah) {
+//return 2037 - birthYeah;
+//};
+
+//ARROW FUNCTION
+const calcAge3 = (birthYeah) => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+	const age = 2037 - birthYeah;
+	const retirement = 65 - age;
+	//return retirement;
+	return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Bravo"));
+console.log(yearsUntilRetirement(1980, "Jonas"));
+
+/*======== FUNCTIONS CALLING OTHER FUNCTIONS========== */
