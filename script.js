86bdb4cce0587d492d10449dde69db97d0091d46
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-05 13:57:18 -0600

    coding how to control arrays

--- script.js
+++ script.js
@@ -191,3 +191,36 @@ console.log(ages);
 */
 
 /*======== BASIC ARRAY OPERATIONS (METHODS)========== */
+
+const friends = ["Michael", "Steven", "Peter"];
+
+//Add elements
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+//Remove elements
+friends.pop(); //last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Peter")) {
+	console.log("You have a friend called Steven");
+}
+
+/*======== CHALLENGE 2 ========== */
