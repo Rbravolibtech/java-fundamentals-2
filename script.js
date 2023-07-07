1d908982893681e8ba86b048a8c269017a240fd2
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-07 10:47:20 -0600

    challenge code for loops

--- script.js
+++ script.js
@@ -464,7 +464,7 @@ for (let exercise = 1; exercise <= 4; exercise++) {
 }
 */
 /*======== THE WHILE LOOP ========== */
-
+/*
 ////for (let rep = 1; rep <= 9; rep++) {
 //console.log(`Lifting weights repetition ${rep}`);
 //}
@@ -483,4 +483,31 @@ while (dice !== 6) {
 	if (dice === 6) console.log("loop is about to end....");
 }
 
-/*======== FINAL CHALLENGE  ========== */
+/*======== FINAL CHALLENGE 4 ========== */
+
+const calcTip = function (bill) {
+	return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+	const tip = calcTip(bills[i]);
+	tips.push(tip);
+	totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		//sum = sum + arr[i];
+		sum += arr[i];
+	}
+
+	return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
